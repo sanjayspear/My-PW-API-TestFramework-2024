e961b039e2b4d074b55fb4ffb32d10a0692d09ee
sanjayspear <sanjayspear@gmail.com>
2024-05-14 14:25:37 +0530

    Implemented mock api response over actual response

---
+++ tests/workingWithAPI.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+import tags from '../test-data/tags.json';
+
+test.beforeEach(async ({page})=> {
+  await page.route('*/**/api/tags', async route => {
+    await route.fulfill({
+      body: JSON.stringify(tags)
+    })
+  })
+
+  await page.goto('https://angularjs.realworld.io/#/');
+})
+
+test('has title', async ({ page }) => {
+  await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+});
